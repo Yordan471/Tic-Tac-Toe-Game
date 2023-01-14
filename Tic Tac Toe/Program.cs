b766f5337161e29947ed564f7ff4926addfa5fea
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-14 09:11:30 +0200

    Adding players input choice and printing the Tic Tac Toe Board

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,64 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            char[,] ticTacToeBoard = new char[,]
+            {
+                { '_', '_', '_' },
+                { '_', '_', '_' },
+                { '_', '_', '_' }
+            };
+
+            int player = 1;
+
+            while (true)
+            {
+                PrintTicTacToeBoard(ticTacToeBoard);
+                Console.WriteLine();
+                Console.WriteLine($"Player {(player == -1? '1' : '2')}'s turn");
+                Console.WriteLine("Row: (0-2): ");
+                int row = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Column: (0-2): ");
+                int col = int.Parse(Console.ReadLine());
+
+                if (row < 0 || row > ticTacToeBoard.GetLength(0) ||
+                    col < 0 || col > ticTacToeBoard.GetLength(1))
+                {
+                    Console.WriteLine("Invalid coordinates!");
+                    Console.WriteLine("Try again...");
+                    continue;
+                }
+
+                char symbolForPlayer = 'X';
+
+                if (player == 2)
+                {
+                    symbolForPlayer = 'O';
+                }
+
+                ticTacToeBoard[row, col] = symbolForPlayer;
+                // Clearing the Console
+                Console.Clear();                
+
+                player = -1;
+            }
+
+            
+        }
+
+        private static void PrintTicTacToeBoard(char[,] ticTacToeBoard)
+        {
+            Console.WriteLine("Tic Tac Toe Board");
+
+            for (int row = 0; row < ticTacToeBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < ticTacToeBoard.GetLength(1); col++)
+                {
+                    Console.Write(ticTacToeBoard[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
